73f2e3d68eed0f3623bc57cc97f8c9bbff0abda1
Ed J <mohawk2@users.noreply.github.com>
2018-10-21 19:31:11 +0100

    strip runtime prereqs and fix min perl

--- lib/JSON/Transform/Grammar.pm
+++ lib/JSON/Transform/Grammar.pm
@@ -1,6 +1,5 @@
 package JSON::Transform::Grammar;
 
-use 5.014;
 use strict;
 use warnings;
 use base 'Pegex::Grammar';
@@ -51,7 +50,7 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
         },
         {
           '-skip' => 1,
-          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*:(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*:(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
         },
         {
           '.ref' => 'exprSingleValue'
@@ -62,14 +61,14 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
       '.all' => [
         {
           '-skip' => 1,
-          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\[(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\[(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
         },
         {
           '.ref' => 'exprSingleValue'
         },
         {
           '-skip' => 1,
-          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\](?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\](?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
         }
       ]
     },
@@ -77,7 +76,7 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
       '.all' => [
         {
           '-skip' => 1,
-          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\@(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\@(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
         },
         {
           '-flat' => 1,
@@ -89,7 +88,7 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
       '.all' => [
         {
           '-skip' => 1,
-          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\#(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\#(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
         },
         {
           '-flat' => 1,
@@ -122,7 +121,7 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
       '.all' => [
         {
           '-skip' => 1,
-          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\{(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\{(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
         },
         {
           '-flat' => 1,
@@ -130,7 +129,7 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
         },
         {
           '-skip' => 1,
-          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\}(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\}(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
         }
       ]
     },
@@ -270,10 +269,10 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
       '.rgx' => qr/\G(\\u[0-9a-fA-F]{4})/
     },
     'opArrayFrom' => {
-      '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*(<\@)(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+      '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*(<\@)(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
     },
     'opCopyFrom' => {
-      '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*(<\-)(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+      '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*(<\-)(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
     },
     'opFrom' => {
       '.any' => [
@@ -286,10 +285,10 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
       ]
     },
     'opMoveFrom' => {
-      '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*(<<)(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+      '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*(<<)(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
     },
     'opObjectFrom' => {
-      '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*(<%)(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+      '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*(<%)(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
     },
     'singleValueMod' => {
       '.any' => [
@@ -355,7 +354,7 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
       '.all' => [
         {
           '-skip' => 1,
-          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
         },
         {
           '.any' => [
@@ -376,7 +375,7 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
         },
         {
           '-skip' => 1,
-          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/u
+          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*/
         }
       ]
     },
@@ -389,7 +388,7 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
       '.all' => [
         {
           '-skip' => 1,
-          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\$/u
+          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\$/
         },
         {
           '.rgx' => qr/\G([A-Z]*)/
@@ -400,7 +399,7 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
       '.all' => [
         {
           '-skip' => 1,
-          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\$/u
+          '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))*\$/
         },
         {
           '.rgx' => qr/\G([a-z][a-zA-Z]*)/
@@ -408,7 +407,7 @@ sub make_tree {   # Generated/Inlined by Pegex::Grammar (0.67)
       ]
     },
     'ws2' => {
-      '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))+/u
+      '.rgx' => qr/\G(?:\s|\x{FEFF}|[\ \t]*\-\-[\ \t]*[^\r\n]*(?:\r?\n|\r!NL|\z))+/
     }
   }
 }
--- lib/JSON/Transform/Parser.pm
+++ lib/JSON/Transform/Parser.pm
@@ -1,16 +1,11 @@
 package JSON::Transform::Parser;
 
-use 5.014;
 use strict;
 use warnings;
 use base qw(Pegex::Parser);
 use Exporter 'import';
-use Return::Type;
-use Types::Standard -all;
-use Function::Parameters;
 use JSON::Transform::Grammar;
 use XML::Invisible::Receiver;
-use JSON::Transform::Error;
 
 use constant DEBUG => $ENV{JSON_TRANSFORM_DEBUG};
 
@@ -47,9 +42,8 @@ method. This achieves hiding of Pegex implementation details.
 =cut
 
 my $GRAMMAR = JSON::Transform::Grammar->new; # singleton
-fun parse(
-  Str $source,
-) :ReturnType(ArrayRef[HashRef]) {
+sub parse {
+  my ($source) = @_;
   my $parser = __PACKAGE__->SUPER::new(
     grammar => $GRAMMAR,
     receiver => XML::Invisible::Receiver->new,
@@ -57,37 +51,6 @@ fun parse(
   );
   my $input = Pegex::Input->new(string => $source);
   scalar $parser->SUPER::parse($input);
-}
-
-=head2 format_error
-
-Override of parent method. Returns a L<JSON::Transform::Error>.
-
-=cut
-
-sub format_error :ReturnType(InstanceOf['JSON::Transform::Error']) {
-    my ($self, $msg) = @_;
-    my $buffer = $self->{buffer};
-    my $position = $self->{farthest};
-    my $real_pos = $self->{position};
-    my ($line, $column) = @{$self->line_column($position)};
-    my $pretext = substr(
-        $$buffer,
-        $position < 50 ? 0 : $position - 50,
-        $position < 50 ? $position : 50
-    );
-    my $context = substr($$buffer, $position, 50);
-    $pretext =~ s/.*\n//gs;
-    $context =~ s/\n/\\n/g;
-    return JSON::Transform::Error->new(
-      locations => [ { line => $line, column => $column } ],
-      message => <<EOF);
-Error parsing Pegex document:
-  msg:      $msg
-  context:  $pretext$context
-            ${\ (' ' x (length($pretext)) . '^')}
-  position: $position ($real_pos pre-lookahead)
-EOF
 }
 
 1;
